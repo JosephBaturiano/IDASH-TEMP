e21f74f0843468ac59d2585d648e4e581636e4bc
crisjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2024-08-16 14:55:31 +0800

    feature: functional add and edit button

--- src/components/TimeSheetCard.jsx
+++ src/components/TimeSheetCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 
-const TimeSheetCard = ({ item }) => (
+const TimeSheetCard = ({ item, onEdit}) => (
     <div key={item.id} className="bg-white shadow-none border-t border-gray-200 hover:bg-gray-50 transition-colors duration-300 p-4 rounded-lg">
         <div className="grid grid-cols-7 gap-4 items-center w-full">
             <div className="flex items-center justify-center text-gray-900 font-medium">{item.taskNumber}</div>
@@ -11,7 +11,7 @@ const TimeSheetCard = ({ item }) => (
             <div className="flex items-center justify-center text-gray-700">{item.withWhom}</div>
             <div className="flex items-center justify-center text-gray-700">{item.deliverables}</div>
             <div className="flex justify-center space-x-2">
-                <button className="bg-blue-100 p-1 rounded-full hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-colors duration-300">
+                <button onClick={() => onEdit(item)} className="bg-blue-100 p-1 rounded-full hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-colors duration-300">
                     <EditIcon className="text-blue-600" />
                 </button>
             </div>
