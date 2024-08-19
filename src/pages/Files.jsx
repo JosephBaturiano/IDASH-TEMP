3b4a6b30733d884c4c99ca4ae4c9c08b7ef73f31
heiimics <70866584+MicaellaSalili@users.noreply.github.com>
2024-08-19 10:31:24 +0800

    feat: add home component to files page

--- src/pages/Files.jsx
+++ src/pages/Files.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Home from './Home'; // Import the Home component
 
 const Files = () => {
   return (
-    <div>
-      <h1>This is files</h1>
-    </div>
-  )
-}
+    <Home>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Files</h1>
+        <p>This is files</p>
+      </div>
+    </Home>
+  );
+};
 
-export default Files
+export default Files;
