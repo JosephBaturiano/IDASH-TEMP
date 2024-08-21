5dac90f87b248a168a0154f568abd453f7969ff2
Ella Mae Lumawag <148688137+ellameaea@users.noreply.github.com>
2024-08-21 18:30:48 +0800

    chore(badges): edit the badges field for better style

--- src/components/ProfileBadges.jsx
+++ src/components/ProfileBadges.jsx
@@ -7,9 +7,9 @@ const ProfileBadges = ({ badgeOne, badgeTwo, badgeThree }) => {
     <div className="bg-[#dbedff] rounded-lg shadow-md w-[600px] h-[170px] p-5">
       <h2 className="text-2xl font-medium mb-2">Badges</h2>
       <div className="flex gap-4 px-1 pt-1">
-        <img src={badgeOne || placeholderBadge} alt="Badge 1" className="h-[90px] w-[90px]" />
-        <img src={badgeTwo || placeholderBadge} alt="Badge 2" className="h-[90px] w-[90px]" />
-        <img src={badgeThree || placeholderBadge} alt="Badge 3" className="h-[90px] w-[90px]" />
+        {badgeOne && <img src={badgeOne} alt="Badge 1" className="h-[90px] w-[90px]" />}
+        {badgeTwo && <img src={badgeTwo} alt="Badge 2" className="h-[90px] w-[90px]" />}
+        {badgeThree && <img src={badgeThree} alt="Badge 3" className="h-[90px] w-[90px]" />}
       </div>
     </div>
   );
